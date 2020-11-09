03448372e689659d367ea836b5a862fa789efbc5
jennieleegruber <65926446+jennieleegruber@users.noreply.github.com>
2020-11-08 22:39:00 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// from data.js
+//create a descriptive variable for the data
 var tableData = data;
 
-// YOUR CODE HERE!
-//select tbody using d3
+// create a variable for tbody you'll append based on the user's iinput
 var tbody = d3.select("tbody");
 
+//create function that prints a table based on the data provided 
 function CreateTable(data){
+//you're saying that in the blank space for tbody you're updating the html with data
 tbody.html("");
+// iterate through the data and take each object in the data array and do something with it
 data.forEach((placeObject) => {
+    //create a variable for the rows of the table you want to append to the webpage usting table row tags
     var row = tbody.append("tr");
+    // take the placeObjects of the data 
     Object.entries(placeObject).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
@@ -16,19 +20,13 @@ data.forEach((placeObject) => {
   });
 };
 
-
-//2 function 1st to build table 
-// 2nd to filter your table 
-//function FilterInput
-
 function UserInput() {
-    var fdata = tableData
+  var fdata = tableData;
 
-    var user_input = d3.select("#datetime").property("value");
-    if(user_input) {
-    fdata = fdata.filter(row => row.datetime === user_input); 
-    }
-
+  var user_input = d3.select("#datetime").property("value");
+  if(user_input) {
+  fdata = fdata.filter(row => row.datetime === user_input); 
+  }
     CreateTable(fdata);
 
 };
